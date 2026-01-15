766db4ae2c59ffce9f92a1815f87837037c0f1f8
Nagy Zétény <nagyzeteny6@gmail.com>
2026-01-15 15:13:44 +0100

    feat: add shadcn/ui

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,24 +3,37 @@ import '~/styles/globals.css';
 import type { Viewport } from 'next';
 import { ClerkProvider } from '@clerk/nextjs';
 import { shadcn } from '@clerk/themes';
+import { ThemeProvider } from 'next-themes';
 import { GeistSans } from 'geist/font/sans';
 
+import { Toaster } from '~/components/ui/sonner';
+
 export const viewport: Viewport = {
-  colorScheme: 'dark',
+  width: 'device-width',
+  initialScale: 1,
+  colorScheme: 'dark light',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider appearance={{ baseTheme: shadcn }}>
-      <html lang="hu" className={`${GeistSans.variable} dark`}>
-        <body>
-          <div className="grid grid-cols-1 grid-rows-[1fr_auto] gap-6">
-            <div className="min-h-screen">{children}</div>
+      <html lang="hu" className={`${GeistSans.variable} scroll-smooth`} suppressHydrationWarning>
+        <body className="overflow-x-hidden antialiased">
+          <ThemeProvider
+            storageKey="theme"
+            defaultTheme="dark"
+            attribute="class"
+            enableColorScheme
+            disableTransitionOnChange
+          >
+            <div className="grid grid-cols-1 grid-rows-[1fr_auto] gap-6">
+              <div className="min-h-screen">{children}</div>
 
-            {/* <Footer /> */}
-          </div>
+              {/* <Footer /> */}
+            </div>
 
-          {/* <Toaster /> */}
+            <Toaster />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
