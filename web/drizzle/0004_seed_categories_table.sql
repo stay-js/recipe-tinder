d9dc78828ccba98668c3da2fa790022d58182e8d
Polyák Panna <ppanna2005@gmail.com>
2026-01-15 16:23:36 +0100

    feat: add categories seeder

---
+++ web/drizzle/0004_seed_categories_table.sql
@@ -0,0 +1,12 @@
+-- Custom SQL migration file, put your code below! --
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Reggeli'),
+(2, 'Tízórai/Snack'),
+(3, 'Leves'),
+(4, 'Előétel'),
+(5, 'Főétel'),
+(6, 'Saláta'),
+(7, 'Köret'),
+(8, 'Desszert'),
+(9, 'Szendvics/Wrap');
