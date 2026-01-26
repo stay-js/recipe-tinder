f996c8fa623bbf7e78a3358e2836ecd48dc00097
Nagy Zétény <nagyzeteny6@gmail.com>
2026-01-26 10:35:12 +0100

    feat: add field, label, textarea, customize component styling

--- web/src/components/ui/badge.tsx
+++ web/src/components/ui/badge.tsx
@@ -14,7 +14,8 @@ const badgeVariants = cva(
           'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
         destructive:
           'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        outline: 'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+        outline:
+          'text-foreground bg-background dark:bg-input/30 [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
       },
     },
     defaultVariants: {
